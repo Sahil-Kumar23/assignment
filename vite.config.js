ce5c018436a8f7631ab453495be205a024e78d10
Sahil-Kumar23 <76045890+Sahil-Kumar23@users.noreply.github.com>
2024-02-24 17:59:40 +0530

    in vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://sahil-kumar23.github.io/eshway_assignment/",
+  base: '/eshway_assignment/',
 })
